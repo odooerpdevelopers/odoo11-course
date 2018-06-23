a231f3eae3dcc73d3e2315cea7fdf7c745bb19f6
odooerpdevelopers <odooerpdevelopers@gmail.com>
2018-06-23 08:11:04 +0200

    Add api.constrains examples

--- library/models/library_book.py
+++ library/models/library_book.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 
 
 class LibraryBook(models.Model):
@@ -14,3 +14,16 @@ class LibraryBook(models.Model):
     description = fields.Html(string="Description")
     category_id = fields.Many2one("library.category", string="Category")
 
+    @api.constrains('name')
+    def check_name(self):
+        if not self.name:
+            raise exceptions.ValidationError(
+                "Name must be filled!!!"
+            )
+
+    @api.constrains('pages')
+    def check_pages(self):
+        if self.pages <= 0:
+            raise exceptions.ValidationError(
+                "Pages must be > 0!!!"
+            )
